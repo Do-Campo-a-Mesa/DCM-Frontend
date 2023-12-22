206cff2294ba0f5c3b5e838ff053cfc1b46862e4
efrainmpp1 <efrainmpp@hotmail.com>
2023-12-22 14:19:55 -0300

    Move App base page to Home Page path

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
+import Home from './screens/Home/';
 import { NotFound_404 } from './screens/NotFound_404';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound_404 />} />
       </Routes>
     </Router>
