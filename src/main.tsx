84000fd291018a01567cd1314b36bf564964dfb7
efrainmpp1 <efrainmpp@hotmail.com>
2023-12-29 16:06:25 -0300

    add store redux provider into system

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material';
 import Theme from './lib/theme';
+import { Provider } from 'react-redux';
+import { store } from './lib/store';
 import AppRouter from './router';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <Theme>
-        <AppRouter />
-      </Theme>
-    </StyledEngineProvider>
+    <Provider store={store}>
+      <StyledEngineProvider injectFirst>
+        <Theme>
+          <AppRouter />
+        </Theme>
+      </StyledEngineProvider>
+    </Provider>
   </React.StrictMode>
 );
