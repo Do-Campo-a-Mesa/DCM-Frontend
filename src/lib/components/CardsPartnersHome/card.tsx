a89a842e0d53942eddfd1fc6bab74072e96605b4
efrainmpp1 <efrainmpp@hotmail.com>
2024-02-15 18:21:16 -0300

    remove un used <></>

--- src/lib/components/CardsPartnersHome/card.tsx
+++ src/lib/components/CardsPartnersHome/card.tsx
@@ -18,43 +18,41 @@ const CardPartnersHome: React.FC<Props> = ({ partner }) => {
   const theme = useTheme();
 
   return (
-    <>
-      <div>
-        <Card sx={{ display: 'flex', p: 2, border: 2, mb: 2 }}>
-          <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-            <img
-              src={partner.logotipo}
-              alt={partner.companyName}
-              style={{ width: '45px', height: '45px' }}
-            />
-          </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <CardContent sx={{ flex: '1 0 auto' }}>
-              <Typography
-                component="div"
-                sx={{
-                  fontFamily: theme.customTypography.fontFamily,
-                  typography: theme.customTypography.h4,
-                  color: theme.customPalette.primary.black,
-                }}
-              >
-                {partner.companyName}
-              </Typography>
-              <Typography
-                component="div"
-                sx={{
-                  fontFamily: theme.customTypography.fontFamily,
-                  typography: theme.customTypography.h8,
-                  color: theme.customPalette.neutral.secondaryText,
-                }}
-              >
-                {partner.business}
-              </Typography>
-            </CardContent>
-          </Box>
-        </Card>
-      </div>
-    </>
+    <div>
+      <Card sx={{ display: 'flex', p: 2, border: 2, mb: 2 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+          <img
+            src={partner.logotipo}
+            alt={partner.companyName}
+            style={{ width: '45px', height: '45px' }}
+          />
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography
+              component="div"
+              sx={{
+                fontFamily: theme.customTypography.fontFamily,
+                typography: theme.customTypography.h4,
+                color: theme.customPalette.primary.black,
+              }}
+            >
+              {partner.companyName}
+            </Typography>
+            <Typography
+              component="div"
+              sx={{
+                fontFamily: theme.customTypography.fontFamily,
+                typography: theme.customTypography.h8,
+                color: theme.customPalette.neutral.secondaryText,
+              }}
+            >
+              {partner.business}
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </div>
   );
 };
 
