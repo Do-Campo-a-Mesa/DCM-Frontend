aa5c8781b691d003d5b2bb7662c55aa20e0c294d
Fernando Lucas <nandolukss@gmail.com>
2024-04-04 17:58:28 -0300

    message sending logic

--- src/screens/ContactUs/Components/Form/form.tsx
+++ src/screens/ContactUs/Components/Form/form.tsx
@@ -1,11 +1,37 @@
 import { Button, TextField } from '@mui/material';
 import { useCustomStyles } from './style';
+import { useState } from 'react';
 
 const Form: React.FC = () => {
   const style = useCustomStyles();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { id, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Integração com a API
+    console.log('Dados do formulário:', formData);
+
+    setFormData({
+      name: '',
+      email: '',
+      message: '',
+    });
+  };
   return (
-    <form style={{ textAlign: 'center' }}>
+    <form onSubmit={handleSubmit}>
       <TextField
         required
         id="name"
@@ -13,6 +39,8 @@ const Form: React.FC = () => {
         variant="outlined"
         fullWidth
         margin="normal"
+        value={formData.name}
+        onChange={handleChange}
         sx={style.TextFieldStyle}
       />
       <TextField
@@ -22,6 +50,8 @@ const Form: React.FC = () => {
         variant="outlined"
         fullWidth
         margin="normal"
+        value={formData.email}
+        onChange={handleChange}
         sx={style.TextFieldStyle}
       />
       <TextField
@@ -33,9 +63,11 @@ const Form: React.FC = () => {
         rows={4}
         fullWidth
         margin="normal"
+        value={formData.message}
+        onChange={handleChange}
         sx={style.TextFieldStyle}
       />
-      <Button variant="contained" sx={style.ButtonStyle}>
+      <Button variant="contained" type="submit" sx={style.ButtonStyle}>
         ENVIAR MENSAGEM
       </Button>
     </form>
