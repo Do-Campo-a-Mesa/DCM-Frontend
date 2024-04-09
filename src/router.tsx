d38741173fc9cd6763f00203cc742b8237100956
Fernando Lucas <nandolukss@gmail.com>
2024-04-08 21:24:39 -0300

    route faq page

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import UserSearchProducts from './screens/UserSearchProducts';
 import SingleProduct from './screens/SingleProduct';
 import ContactUs from './screens/ContactUs';
 import AboutUs from './screens/AboutUs';
+import FAQPage from './screens/FAQ';
 
 const AppRouter = () => {
   return (
@@ -16,6 +17,7 @@ const AppRouter = () => {
         <Route path="/produto/:id" element={<SingleProduct />} />
         <Route path="/contactus" element={<ContactUs />} />
         <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/faq" element={<FAQPage />} />
       </Routes>
     </Router>
   );
