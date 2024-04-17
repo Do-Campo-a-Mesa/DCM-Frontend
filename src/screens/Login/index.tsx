d6139e58cc0a190f05302a36d8e52096ece3a8c1
efrainmpp1 <efrainmpp@hotmail.com>
2024-04-17 17:23:16 -0300

    fix: login screen with correct Title

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -2,7 +2,7 @@ import LoginForm from './loginForm';
 import LayoutRegisterOrLogin from '../../lib/layout/login_and_register';
 
 const Login: React.FC = () => {
-  return <LayoutRegisterOrLogin formComponent={<LoginForm />} />;
+  return <LayoutRegisterOrLogin isLogin formComponent={<LoginForm />} />;
 };
 
 export default Login;
