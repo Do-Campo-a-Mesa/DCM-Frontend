94c313eace32bb09abbbefc59623cc4efb678207
efrainmpp1 <efrainmpp@hotmail.com>
2024-04-19 17:26:12 -0300

    feature: create user account integration with service

--- src/screens/UserRegister/user_register_form.tsx
+++ src/screens/UserRegister/user_register_form.tsx
@@ -1,19 +1,45 @@
 import { Button, TextField } from '@mui/material';
-//import { useCustomStyles } from '../style';
 import { useForm } from 'react-hook-form';
 import { CreateUserInput } from '../../lib/interfaces/User';
+import { createUserAccount } from '../../services/user';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const UserRegisterForm: React.FC = () => {
-  //const style = useCustomStyles();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
+    watch, // Use o watch para monitorar o campo de repetir senha
   } = useForm<CreateUserInput>();
 
-  const onSubmit = (formData: CreateUserInput) => {
-    // Integração com a API ou qualquer outra lógica de submissão de formulário
-    console.log('Dados do formulário:', formData);
+  const [errorMessage, setErrorMessage] = useState<string>(''); // Use o useState para armazenar a mensagem de erro
+
+  const navigate = useNavigate();
+
+  const password = watch('password'); // Obtém o valor do campo de senha
+  const repeatPassword = watch('repeat_password'); // Obtém o valor do campo de repetir senha
+
+  const onSubmit = async (formData: CreateUserInput) => {
+    if (password !== repeatPassword) {
+      setErrorMessage('As senhas precisam ser iguais.'); // Define a mensagem de erro
+      return; // Aborta o envio do formulário se as senhas não forem iguais
+    }
+
+    await createUserAccount(formData)
+      .then((response) => {
+        if (response.status == 201) {
+          // Redireciona para a rota de login
+          navigate('/login');
+        }
+      })
+      .catch(() => {
+        setErrorMessage(
+          'Houver um erro ao criar sua conta, por favor verificar os dados inseridos'
+        ); // Define a mensagem de erro
+        reset(); // Limpa os campos do formulário
+      });
   };
 
   return (
@@ -27,7 +53,6 @@ const UserRegisterForm: React.FC = () => {
         margin="normal"
       />
       {errors.email && <span>Este campo é obrigatório</span>}
-
       <TextField
         {...register('password', { required: true })}
         id="password"
@@ -38,7 +63,6 @@ const UserRegisterForm: React.FC = () => {
         type="password"
       />
       {errors.password && <span>Este campo é obrigatório</span>}
-
       <TextField
         {...register('repeat_password', { required: true })}
         id="repeat_password"
@@ -49,7 +73,8 @@ const UserRegisterForm: React.FC = () => {
         type="password"
       />
       {errors.password && <span>Este campo é obrigatório</span>}
-
+      {errorMessage && <span>{errorMessage}</span>}{' '}
+      {/* Exibe a mensagem de erro, se houver */}
       <Button variant="contained" type="submit">
         ENVIAR MENSAGEM
       </Button>
