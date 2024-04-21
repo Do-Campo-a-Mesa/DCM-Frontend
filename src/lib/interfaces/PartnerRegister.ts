2063044bc90e6b38f88518d0f5386f2b681cf486
Fernando Lucas <nandolukss@gmail.com>
2024-04-21 13:07:35 -0300

    update

--- src/lib/interfaces/PartnerRegister.ts
+++ src/lib/interfaces/PartnerRegister.ts
@@ -12,34 +12,7 @@ export interface PartnerForm {
   numero: number;
   cidade: string;
   bairro: string;
-  estado:
-    | 'AC'
-    | 'AL'
-    | 'AP'
-    | 'AM'
-    | 'BA'
-    | 'CE'
-    | 'DF'
-    | 'ES'
-    | 'GO'
-    | 'MA'
-    | 'MT'
-    | 'MS'
-    | 'MG'
-    | 'PA'
-    | 'PB'
-    | 'PR'
-    | 'PE'
-    | 'PI'
-    | 'RJ'
-    | 'RN'
-    | 'RS'
-    | 'RO'
-    | 'RR'
-    | 'SC'
-    | 'SP'
-    | 'SE'
-    | 'TO';
+  estado: string;
   complemento: string;
   entrega: boolean;
   entregaTipo: DeliveryOption[];
