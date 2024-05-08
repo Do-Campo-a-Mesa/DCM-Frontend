f562f9fd350be70cf2ddd61232ab4cf75d334764
efrainmpp1 <efrainmpp@hotmail.com>
2024-05-08 09:46:06 -0300

    add service to register Partner on system

--- src/lib/interfaces/PartnerRegister.ts
+++ src/lib/interfaces/PartnerRegister.ts
@@ -19,3 +19,23 @@ export interface PartnerForm {
   entregaDias: string[];
   tempoProcessamento: number;
 }
+
+export interface StoreModel {
+  owner_user_id: string;
+  cnpj: string;
+  name: string;
+  short_description: string;
+  uf: string;
+  city: string;
+  bairro: string;
+  cep: string;
+  address: string;
+  business_address: string;
+  phone: string;
+  email: string;
+  instagram: string;
+  minimum_order_value: number;
+  minimum_value_for_free_shipping: number;
+  order_processing_time: number;
+  delivery_logistics: string;
+}
