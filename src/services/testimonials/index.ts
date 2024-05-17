13a637f5fc3ceff086aa21598100ee74f1b7f379
efrainmpp1 <efrainmpp@hotmail.com>
2024-05-17 20:45:59 -0300

    put testimonials on home page on mock temporarily

--- src/services/testimonials/index.ts
+++ src/services/testimonials/index.ts
@@ -1,8 +1,8 @@
 import * as mock from './mock';
-import * as integration from './integration';
+//import * as integration from './integration';
 import { TestimonialsResponse } from '../../lib/interfaces/Testimonial';
 
-const useMock = process.env.NODE_ENV === 'mock';
+//const useMock = process.env.NODE_ENV === 'mock';
 
 export const getHomePageTestimonials: () => Promise<TestimonialsResponse> =
-  useMock ? mock.getHomePageTestimonials : integration.getHomePageTestimonials;
+  mock.getHomePageTestimonials;
